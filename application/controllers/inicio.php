74bd95003f81df3a58904f2ec5e617b26391f7e8
miguel <m.alejandro_80@hotmail.com>
2018-11-30 19:08:11 -0800

    plantilla inicial

--- application/controllers/inicio.php
+++ application/controllers/inicio.php
@@ -5,12 +5,12 @@ class Inicio extends CI_controller{
 
 		public function index(){
 		$data = array('titulo' => 'saludos', 'mensaje' => 'fuck you');
-		$this->load->view("inicio",$data);
+        $this->load->view("/template/head");
+        $this->load->view("/template/nav");
+        $this->load->view("/template/header");
+        $this->load->view("/template/content");
+        $this->load->view("/template/footer");
+		//$this->load->view("inicio",$data);
 			
-	}
-
-	public function link(){
-		
-		echo 'presionaste el link';	
 	}
 }
